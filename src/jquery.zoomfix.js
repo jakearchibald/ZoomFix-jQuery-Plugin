583a07bdf0f109913156b0824864d15694d87e41
Jake Archibald <jakearchibald@theteam.co.uk>
2010-09-26 20:18:42 +0100

    Speeding things up with a bit of caching

--- src/jquery.zoomfix.js
+++ src/jquery.zoomfix.js
@@ -1,6 +1,7 @@
 (function($) {
 	var oldCurCssFn = $.curCSS,
 		testSize = 1000,
+		cache = {},
 		$testElm = $('<div/>').css({
 			position: 'absolute',
 			top: testSize,
@@ -12,6 +13,11 @@
 			padding: 0
 		});
 	
+	// when zoom values changes a resize event is fired, invalidate the cache
+	$(window).resize(function() {
+		cache = {};
+	});
+	
 	// Some browsers mis-report computedStyle when the browser is zoomed
 	$.curCSS = function(element, prop, force) {
 		// only supports certain properties at the moment
@@ -20,16 +26,22 @@
 			return oldCurCssFn.apply(this, arguments);
 		}
 		
-		// get value from test element
-		var testValue = oldCurCssFn( $testElm.prependTo(document.body)[0], prop, true ),
-			// how far is it off what we expect?
-			errorCoefficient = testSize / parseFloat(testValue),
+		// try and get the value from the cache
+		var errorCoefficient = cache[prop],
 			// get value from jquery
 			calculatedValue = oldCurCssFn.apply($, arguments),
 			// get unit
 			unit = /[a-zA-Z%]*$/.exec(calculatedValue),
 			// get number value
-			parsedValue = parseFloat(calculatedValue);
+			parsedValue = parseFloat(calculatedValue),
+			testValue;
+		
+		if ( !errorCoefficient ) {
+			// get value from test element
+			testValue = oldCurCssFn( $testElm.prependTo(document.body)[0], prop, true );
+			// how far is it off what we expect?
+			errorCoefficient = cache[prop] = testSize / parseFloat(testValue);
+		}
 		
 		$testElm.detach();
 
